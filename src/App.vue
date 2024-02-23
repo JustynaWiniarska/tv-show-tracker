e08ac6ad5d616d61aedbf258dc2c08b0ff0cbdfc
Justyna <justyna.field@gmail.com>
2024-02-23 13:51:02 -0600

    Installs packages

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Music Library</h1>
+    <font-awesome-icon icon="coffee" />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-Vue.config.productionTip = false
+import 'bulma/css/bulma.css'
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faCoffee)
+
+const app = createApp(App)
+
+// Configuration
+app.config.productionTip = false
+
+// Use the router
+app.use(router)
+
+
+
+// Use FontAwesome globally
+app.component('font-awesome-icon', FontAwesomeIcon)
+
+
+// Mount the app
+app.mount('#app')
+
+
